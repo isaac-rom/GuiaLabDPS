430cb37af9de8c764bf1bc098869d78d2521a725
isaac-rom <isaac60romero@gmail.com>
2023-08-18 16:52:33 -0600

    Create App.jsx

---
+++ guia02/Discusion/product-list/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ProductList } from './components/ProductList';
+
+function App() {
+  return (
+    <>
+    
+    <ProductList/>
+    </>
+  )
+}
+
+export default App
